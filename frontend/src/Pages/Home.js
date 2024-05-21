cd59671f0f2318f41b3c2e401de83fd7e71a86f7
Rishi Airan <20bcs042@ietdavv.edu.in>
2024-05-22 00:16:36 +0530

    added more components

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import SelectMovie from '../Components/SelectMovie'
 import '../Css/Home.css'
+import TimeSchedule from '../Components/TimeSchedule'
+import SelectSeats from '../Components/SelectSeats'
+import LastBookingDetails from '../Components/LastBookingDetails'
+
+
 const Home = () => {
   return (
+    <div className='container'>
     <div>
       <div className='select_movie_component'>
       <SelectMovie />
+      </div>
+      <div>
+        <LastBookingDetails/>
+      </div>
+      </div>
+      <div>
+        <TimeSchedule />
+        <SelectSeats />
+        <button>Book now</button>
       </div>
     </div>
   )
